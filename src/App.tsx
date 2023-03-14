10fa5975867b1bd69a5bbc540113b2ad7b949bb8
AndersonAndrad <anderson_andrade_@outlook.com>
2023-03-14 01:13:08 -0300

    feature: add butons to navigate between tables

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,34 @@
-import { Flex } from '@chakra-ui/react';
+import { Button, Flex } from '@chakra-ui/react';
+
+import { useState } from 'react';
 import { Guns } from './components/guns.component';
+import { Locations } from './components/locations.component';
+import { People } from './components/people.components';
 
 export function App() {
+  const [tableToShow, setTableToShow] = useState<tableEnum>(tableEnum.PEOPLE)
+
   return (
-    <Flex flexDirection={'column'} padding={2}>
-      <Guns></Guns>
-      {/* <Locations /> */}
-      {/* <People /> */}
+    <Flex flexDirection={'column'} padding={2} gap={5}>
+      <Flex justifyContent={'space-between'}>
+        <Button onClick={() => { setTableToShow(tableEnum.PEOPLE) }}>Suspeitos</Button>
+
+        <Button onClick={() => { setTableToShow(tableEnum.GUNS) }}>Armas</Button>
+
+        <Button onClick={() => { setTableToShow(tableEnum.LOCATIONS) }}>Locais</Button>
+      </Flex>
+
+      {tableToShow === tableEnum.PEOPLE && <People />}
+
+      {tableToShow === tableEnum.GUNS && <Guns />}
+
+      {tableToShow === tableEnum.LOCATIONS && <Locations />}
     </Flex>
   )
+}
+
+enum tableEnum {
+  GUNS = 'guns',
+  LOCATIONS = 'locations',
+  PEOPLE = 'people'
 }
