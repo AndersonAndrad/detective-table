121b52613db1476d63f2b7c6e777f6262571cb83
AndersonAndrad <anderson_andrade_@outlook.com>
2023-03-14 01:22:23 -0300

    refactor: possibility to select locations and maintain selected

--- src/components/locations.component.tsx
+++ src/components/locations.component.tsx
@@ -1,27 +1,48 @@
-import { Flex } from '@chakra-ui/react';
+import { Flex, Text } from '@chakra-ui/react';
+import { IItem, listLocations } from '../data/items.data';
+
 import { useState } from 'react';
-import { listLocations } from '../data/items.data';
 import { Card } from './card.component';
 
 export function Locations() {
   const [locations, setLocations] = useState(listLocations)
 
-  const changeSelection = (data: any) => {
-    console.log(data)
+  const changeSelection = (data: Omit<IItem, 'name'>) => {
+    const listLocations = locations.map(location => {
+      if (location.id === data.id) location.selected = data.selected;
+
+      return location
+    })
+
+    setLocations(listLocations)
+  }
+
+  const countSelected = () => {
+    return locations.filter(location => location.selected).length
+  }
+
+  const checkSelected = (id: string) => {
+    return locations.find(location => location.id === id && location.selected)?.selected || false
   }
 
   return (
-    <Flex flexDirection={'column'} gap={3}>
-      {locations.map(location => (
-        <Card
-          key={location.id}
-          id={location.id}
-          name={location.name}
-          selected={location.selected}
-          changeSelection={changeSelection}
-        />
-      ))}
+    <Flex flexDirection={'column'} gap={10}>
+      <Flex>
+        <Text fontSize={18}>Encontrados: {countSelected()} / {locations.length}</Text>
+      </Flex>
 
+      <Flex flexDirection={'column'} gap={3}>
+        {locations.map(location => (
+          <Card
+            key={location.id}
+            id={location.id}
+            name={location.name}
+            selected={checkSelected(location.id)}
+            changeSelection={changeSelection}
+          />
+        ))}
+
+      </Flex>
     </Flex>
   )
 }
